32474a31f58850ce27699b0ff500c89db34e5b4f
heehaa25 <black1berrychoi@gmail.com>
2025-10-22 16:56:03 +0900

    feat(reviews): mock 데이터 기반 리뷰 및 하위 컴포넌트 구현

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,3 +1,12 @@
+import { useState } from 'react';
+import mockItems from '../data/mock.json';
+import ReviewList from './ReviewList';
+
 export default function Reviews() {
-  return <div>reviews</div>;
+  const [items, setItems] = useState(mockItems);
+  return (
+    <div>
+      <ReviewList reviews={items} />
+    </div>
+  );
 }
