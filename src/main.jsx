3f6385fb0b46a94e9f4e5a7e2dd4091cd4c8947e
heehaa25 <black1berrychoi@gmail.com>
2025-10-22 13:41:57 +0900

    feat(routing): 주요 페이지 라우팅과 Outlet 구조 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import NotFound from './pages/NotFound.jsx';
+import Home from './pages/Home.jsx';
+import Reviews from './components/Reviews.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/reviews', element: <Reviews /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
