b64a06bf7e3e435f0cdb7a9d65f9ec7350fc0286
heehaa25 <black1berrychoi@gmail.com>
2025-10-23 13:29:43 +0900

    style(bookcard): 북카드 사진 및 텍스트 스타일 적용

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -4,9 +4,13 @@ export default function BookCard({ book, onClick }) {
 
   return (
     <li onClick={() => onClick({ bookname, authors, isbn13, bookImageURL })}>
-      <img src={bookImageURL} alt={bookname} />
-      <h1>{bookname}</h1>
-      <p>{authors}</p>
+      <img
+        className='w-full h-80 object-cover'
+        src={bookImageURL}
+        alt={bookname}
+      />
+      <h1 className='font-semibold line-clamp-1 '>{bookname}</h1>
+      <p className='text-sm opacity-80'>{authors}</p>
     </li>
   );
 }
