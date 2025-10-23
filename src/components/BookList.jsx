d0ac5bebf269ad25d2fac9bc297abbc061de924e
heehaa25 <black1berrychoi@gmail.com>
2025-10-23 13:29:06 +0900

    style(bookList): 북 카드가 반응형으로 보이도록 스타일 적용

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,7 +2,7 @@ import BookCard from './BookCard';
 
 export default function BookList({ items, onClick }) {
   return (
-    <ul>
+    <ul className='mx-auto max-w-6xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-2'>
       {items.map((item) => (
         <BookCard key={item.doc.isbn13} book={item} onClick={onClick} />
       ))}
